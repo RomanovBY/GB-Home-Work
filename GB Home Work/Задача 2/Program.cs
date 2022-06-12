494431d97eab18a236a16e2aa48b2934dc1ac9e5
«ROMANOV <alejandromanov@gmail.com>
2022-06-12 17:03:27 +0300

    Задача 2 и файл гитигнор

---
+++ GB Home Work/Задача 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+Console.WriteLine(number1 + " - максимальное число");
+}
+else 
+{
+    Console.WriteLine(number2 + " - максимальное число");
+}
