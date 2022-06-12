902b20c4e0eff09376a0359202ef2615a83cc330
«ROMANOV <alejandromanov@gmail.com>
2022-06-12 18:10:38 +0300

    Решение задачи 8

---
+++ GB Home Work/Задача 8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index<N+1)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(index + ",");
+        index = index + 1;
+    }
+    else 
+{
+    index = index + 1;
+}
+}
+Console.WriteLine();
