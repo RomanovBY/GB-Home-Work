7c599f1d9e72bded84eecb7d4211569a6f5869fc
«ROMANOV <alejandromanov@gmail.com>
2022-06-16 01:22:40 +0300

    Решение 15 задачи

---
+++ GB Home Work/Задача 15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <=7 && number >=1)
+{
+    if(number <=5)
+Console.WriteLine("Это рабочий день");
+     else
+     {
+        Console.WriteLine("Это выходной день");
+     }
+}
+else 
+{
+    Console.WriteLine("Такого дня не существует");
+   
+}
