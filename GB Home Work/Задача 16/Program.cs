4e23becf6ca21ad8991a2f1ddeb0859a84199786
«ROMANOV <alejandromanov@gmail.com>
2022-06-16 01:25:15 +0300

    Решение доп.16 задачи

---
+++ GB Home Work/Задача 16/Program.cs
@@ -0,0 +1,30 @@
+﻿int firstNumber = Input("Введите первое число: ");
+int secondNumber = Input("Введите второе число: ");
+
+int firstNumberSquare = firstNumber * firstNumber;
+int secondNumberSquare = secondNumber * secondNumber;
+
+if (firstNumber == secondNumberSquare || secondNumber == firstNumberSquare)
+{
+    if (firstNumber == secondNumberSquare)
+    {
+        Console.WriteLine(firstNumber + " является квадратом " + secondNumber);
+
+    }
+    else 
+    {
+        Console.WriteLine(secondNumber + " является квадратом " + firstNumber);
+
+    }
+}
+else 
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
