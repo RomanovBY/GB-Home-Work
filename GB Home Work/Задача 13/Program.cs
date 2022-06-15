b5751d005e8545a4ded2cada208cd39e37a0599d
«ROMANOV <alejandromanov@gmail.com>
2022-06-16 00:54:53 +0300

    Решение 13 задачи с циклом

---
+++ GB Home Work/Задача 13/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(0, 100000);
+Console.WriteLine(number);
+
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    while(number>999)
+    {
+        number = number / 10;
+    }
+    int result = number % 100 % 10;
+    Console.WriteLine(result);
+}
