6a04d675c614f3c46c62b72a521e3303aa24d499
«ROMANOV <alejandromanov@gmail.com>
2022-06-20 17:11:57 +0300

    Решение задачи 19

---
+++ GB Home Work/Задача 19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+int number1 = number;
+int n1 = 0;
+while (number > 0)
+{
+  n1 = number % 10;
+  n = n*10 + n1;
+  number = number/10;
+}
+  if (number1 == n)
+  {
+    Console.WriteLine("Это число является полиндромом!");
+  }
+  else 
+  {
+        Console.WriteLine("Это число не является полиндромом!");
+  }
