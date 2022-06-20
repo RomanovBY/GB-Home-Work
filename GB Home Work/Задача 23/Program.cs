9899c900d059492602a120a08f1d270284725d99
«ROMANOV <alejandromanov@gmail.com>
2022-06-20 17:40:19 +0300

    Решение задачи 23

---
+++ GB Home Work/Задача 23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N: ");
+int count = 1;
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Cube (int a)
+{
+    return a * a * a;
+}
+
+while (count < n) 
+{
+  int result = Cube(count);
+  count++;
+  Console.Write(result + ", ");
+}
+Console.WriteLine (Cube (n));
