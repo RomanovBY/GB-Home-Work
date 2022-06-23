3170942f8208e796c814d95ffce021f1802a8c16
«ROMANOV <alejandromanov@gmail.com>
2022-06-23 19:12:55 +0300

    Решение задачи 27

---
+++ GB Home Work/Задача 29/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] array = new int[8];
+
+PrintArray(array);
+
+void PrintArray(int[] aray)
+{
+    for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine(" ");
+}
