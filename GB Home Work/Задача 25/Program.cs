150cc46adfe890d8e8d293ac7a8e8532ed1e4fab
«ROMANOV <alejandromanov@gmail.com>
2022-06-23 17:05:03 +0300

    Решение задачи 25

---
+++ GB Home Work/Задача 25/Program.cs
@@ -0,0 +1,13 @@
+﻿int Input (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = Input ("Введите число A: ");
+int b = Input ("Введите число B: ");
+int sum = 1;
+for(int i = 1; i <= b; i++)
+{
+  sum = sum * a;
+}
+Console.WriteLine(sum);
