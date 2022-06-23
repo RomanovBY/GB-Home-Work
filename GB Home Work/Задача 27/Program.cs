d3930e430a7ea680b171ad421bb02b0968317757
«ROMANOV <alejandromanov@gmail.com>
2022-06-23 17:37:28 +0300

    Решение задачи 27

---
+++ GB Home Work/Задача 27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int b = a;
+
+while (a > 0)
+{
+    b = a % 10;
+    sum = sum + b;
+    a = a / 10;
+}
+
+Console.WriteLine(sum);
