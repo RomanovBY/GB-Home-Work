51e8d0ce337bf19369d64aaa5f88276fd7a50686
«ROMANOV <alejandromanov@gmail.com>
2022-07-04 14:28:05 +0300

    Решение задачи 34

---
+++ GB Home Work/Задача 34/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = new int[10];
+int count = 0;
+
+for (int i = 0; i < array.Length;i++)
+{
+    array[i] = new Random().Next(100,1000);
+}
+
+
+PrintArray(array);
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2==0)
+        count++;
+}
+Console.WriteLine($"Количество четных чисел в массиве = {count}");
