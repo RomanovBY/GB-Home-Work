0233dfbed55cb7dd7e8ee126bfa4519834c6063b
«ROMANOV <alejandromanov@gmail.com>
2022-07-11 17:46:50 +0300

    Решение задачи 36

---
+++ GB Home Work/Задача 36/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] array = new int[5];
+int SUM = 0;
+int count = array.Length;
+
+for (int i = 0; i < count;i++)
+{
+    array[i] = new Random().Next(1,100);
+}
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
+
+for (int i = 0; i < count;i++)
+{
+    if (i %2==1)
+       SUM = SUM + array[i];
+}
+    Console.WriteLine($"Сумма нечетных элментов в массиве - {SUM}");
+
+
+  
